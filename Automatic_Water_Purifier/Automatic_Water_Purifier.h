eb38229de1c0c5d3c99f471bee42f75430a75a5a
Jerry <jerrymbc@gmail.com>
2021-07-04 20:06:52 +0530

    Adding Files Updated

--- Automatic_Water_Purifier/Automatic_Water_Purifier.h
+++ Automatic_Water_Purifier/Automatic_Water_Purifier.h
@@ -14,11 +14,19 @@
   */
 #define Enable_Debug_Support STD_ON
 
-#define Button_Feed_Back_Enabled STD_ON
-
 /*Reserve Max debug variable sizes as 1KB.*/
-#define Max_Debug_Buffer 128
-#define Max_Debug_Time_Buffer 30
+#define Max_Debug_Buffer 1024
+#define Max_Debug_Time_Buffer 128
+
+/* Following are the Configurations related to the Networking.*/
+/* Default name for recovery Access Point.*/ 
+/* If configured WiFi are not available then shall Automaticaly switch to below mentioned AP. 
+   And after login into this access point you can configure new WiFi Pasword if needed.
+   User name and pasword of AP is not configuriable because its a recovery mechanism.
+   */
+#define AP_Name "Automatic_Water_Purifier"
+#define AP_Pasword "IamPayyampallil"
+
 
 
 /*******************************************************************************
@@ -37,6 +45,17 @@
 #define But_Speed_3 7 /* Button connected to Red wire. For Speed 3 Fan*/
 
 #define But_Bukb 8 /* Button connected to Brown wire. For ON /OFF bulb*/
+
+
+
+
+/*******************************************************************************
+ *  Following are the configuration for EEPROM
+*******************************************************************************/
+/* This Macro represent the maximum available, For ESP32, Its 512Bytes */
+#define Max_Available_EEPROM 512
+
+
 
 /*-----------------------------------------------------------------------------
  *  Configuration END
@@ -69,5 +88,36 @@
 /*******************************************************************************
  *  Enums and typedefs
 *******************************************************************************/
+/*Following is the Enum for NVM Paramater ID.*/
+enum  NVMParam_ID_Enum{
+  NVM_ID_Value_WiFiSSIDName,                     /* This NVM paramater is storing the WiFi SSID Name set by the user.*/
+  NVM_ID_Value_WiFiSSIDPasword,                  /* This NVM paramater is storing the WiFi SSID Pasword set by the user. Pasword shall Not store Securely*/
+  NVM_ID_Value_WiFiStsticIP,                     /* This NVM paramater is storing the Server IP, If user specify then only same shall be considered, Else shall ignore.*/
+  NVM_ID_Value_WiFiServerName,                   /* This NVM paramater is storing the Server Custom Server Name, If user want to, Else Shall use default Server Name.*/
+
+  NVM_ID_Calibration_FlowMeaterFactor,             /* This NVM paramater is storing the convertion factor for the flow meter.*/
+  NVM_ID_Calibration_LowFlowRate,                  /* This NVM paramater is storing the Lowest Flow rate allowed without any warning.*/
+  NVM_ID_Calibration_HighFlowRate,                 /* This NVM paramater is storing the Higest Flow rate allowed without any warning.*/
+  NVM_ID_Calibration_HighPresureCollingTime,       /* This NVM paramater is storing the Cool off time in Second after High presure is being detected.*/
+  NVM_ID_Calibration_WaterTankOverflowCapacity,    /* This NVM paramater is storing the Maximum Tank Capacity. After reaching this level if float sensor is not detected, Then will take action*/
+      
+  NVM_ID_Seting_WaterTankOverflowAction,          /* This NVM paramater is storing Action to me considered once Potential Overflow is detected.*/
+  NVM_ID_Seting_HighPresureAction,                /* This NVM paramater is storing Action to me considered once High presure is detected detected.*/
+  NVM_ID_Seting_OperatationMode,                  /* This NVM paramater is storing Operatation mode of the filter.*/
+  NVM_ID_Seting_LowFlowRateWarningAction,         /* This NVM paramater is storing Action to me considered once low Flow rate is detected.*/
+  NVM_ID_Seting_HighFlowRateWarningAction        /* This NVM paramater is storing Action to me considered once High Flow rate is detected.*/
+  
+};
+
+/* Structure to Configuration paramaters for NVM Paramaters.*/
+typedef struct NVM_Param_Config_Table_Tag {
+   NVMParam_ID_Enum NVMParam_ID;     /* Variable to st*/
+
+
+}NVM_Param_Config_Table_Type;
+
+
+
+
 
 #endif /* End of  Automatic_Water_Purifier_H */
